f0c66fdd282e2f638368df23a9cd1bc0210de21e
Vic Lau <xingyulliiuu@gmail.com>
2025-06-18 17:00:32 +0800

    chore: update resource

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://b.oss.viclau.top/chat/avatar.jpg',
+      name: 'YourName',
+      description: 'Star on <a href="https://github.com/xingyuli/social-binding-zig" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
